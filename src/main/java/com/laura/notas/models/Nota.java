8525cf5319ebf92124a2c703f1f6efe04a5f3d4c
Laura Haro <lauraharomolina@gmail.com>
2023-02-22 12:22:22 +0100

    arreglo entidad

--- src/main/java/com/laura/notas/models/Nota.java
+++ src/main/java/com/laura/notas/models/Nota.java
@@ -2,9 +2,11 @@ package com.laura.notas.models;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Nota {
     @Id
     @GeneratedValue
