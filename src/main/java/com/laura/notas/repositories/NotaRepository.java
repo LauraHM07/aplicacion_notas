e5164af10c33a82a40dfebfad29db23e21fd3084
Laura Haro <lauraharomolina@gmail.com>
2023-03-03 13:37:11 +0100

    cambios

--- src/main/java/com/laura/notas/repositories/NotaRepository.java
+++ src/main/java/com/laura/notas/repositories/NotaRepository.java
@@ -4,11 +4,9 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.laura.notas.models.Nota;
 
-@Repository
 public interface NotaRepository extends JpaRepository<Nota, Integer>{
     List<Nota> findByTituloContainingAndFechaGreaterThan(String titulo, Date fecha);
 }
